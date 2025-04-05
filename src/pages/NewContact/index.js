2240fd78184f28e7476c9e8458d260c17986496c
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-04-05 00:18:16 -0300

    Componentes de input e select

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,5 +1,21 @@
 import PageHeader from "../../components/PageHeader";
 
+import { Input } from "../../components/Utils/Input";
+import { Select } from "../../components/Utils/Select";
+
 export default function NewContact() {
-  return <PageHeader title="Novo Contato" />;
+  return (
+    <>
+      <PageHeader title="Novo Contato" />
+
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="123">Instagram</option>
+        <option value="456">Facebook</option>
+        <option value="789">Twitter</option>
+        <option value="1011">TikTok</option>
+        <option value="1213">Orkut</option>
+      </Select>
+    </>
+  );
 }
