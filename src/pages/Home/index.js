ad9763a747f770000c7838ede9ed1f1dac5c4c1d
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-06-01 22:22:26 -0300

    feat: implementação do component de Loader

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,11 +12,14 @@ import arrow from "../../assets/images/icons/seta.svg";
 import edit from "../../assets/images/icons/edit.svg";
 import trash from "../../assets/images/icons/lixe.svg";
 import Modal from "../../components/Modal";
+import Loader from "../../components/Loader";
 
 export default function Home() {
   return (
     <Container>
-      <Modal danger />
+      {/* <Modal danger /> */}
+
+      <Loader />
 
       <InputSearchContainer>
         <input type="text" placeholder="Pesquisar contato..." />
